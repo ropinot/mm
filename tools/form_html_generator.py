861f8d89a6f5c90d819e7d9f0ff49688a96dcadf
roberto <roberto@local.host>
2014-10-27 10:24:44 +0100

    modified:   form_html_generator.py
    Stampa su file

--- tools/form_html_generator.py
+++ tools/form_html_generator.py
@@ -1,13 +1,12 @@
 import sys
 from myhtmlgen import *
+from lxml import etree, html
 
 if len(sys.argv)< 2:
         print "Indicare il file sorgente per il form"
         sys.exit(1)
 
-html = []
-
-n = {'1': 'one',
+n = {'1': 'one',        # convert numbers to strings
      '2': 'two',
      '3': 'three',
      '4': 'four',
@@ -21,25 +20,49 @@ n = {'1': 'one',
      '12': 'twelve'
 }
 
+generated_html = []
 
 with open(sys.argv[1], 'r') as f:
         for row in f.readlines():
 
                 token = row.lstrip().rstrip().split()
+
                 if token:       # check for empty lines
                         if token[0] == '!':     # ! indicates a new row
                                 new_row = DIV(Class='row')
+
                                 for i in xrange(1, len(token), 2):
                                         new_row <= DIV(token[i], Class='{} twelfths'.format(n[token[i+1]]))
-                                html.append(new_row)
+                                generated_html.append(new_row)
 
                         elif token[0] == '#':    # # for comments
                                 continue
 
-                        else:
+                        else:   # no leading special char (! or #) so continues the preceeding line
                                 for i in xrange(0, len(token), 2):
                                         new_row <= DIV(token[i], Class='{} twelfths'.format(n[token[i+1]]))
-                                html.append(new_row)
+                                generated_html.append(new_row)
 
-for h in html:
-        print h, '\n'
+
+content = ''
+
+for h in generated_html:
+        content += h.__str__()
+
+with open('form.html', 'w') as f:
+        content = html.fromstring(content)
+        f.write(etree.tostring(content, encoding='unicode', pretty_print=True))
+
+
+# f = open('form.html', 'w')
+# for h in generated_html:
+#         f.write(h.__str__())
+#
+# with open('form.html', 'r') as f:
+#         content = f.read()
+#         content = html.fromstring(content)
+#         print(etree.tostring(content, encoding='unicode', pretty_print=True))
+
+
+
+
