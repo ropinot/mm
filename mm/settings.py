91675226d352ba1c870b055359fe37222d0d82cd
Roberto <roberto@local.host>
2015-01-25 15:38:16 +0100

    modified:   mm/settings.py 	modified:   mm/settings.pyc 	modified:   mmlog/urls.pyc 	modified:   mmmain/urls.pyc

--- mm/settings.py
+++ mm/settings.py
@@ -90,6 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/mm/static/'
 
 PROJECT_DIR = os.path.dirname(__file__)
 
