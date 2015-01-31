846fa2ab94d8657f884050137e1dc8f1dc9f9a1c
Roberto <roberto@local.host>
2015-01-31 15:48:05 +0100

    modified:   mmlog/admin.py

--- mmlog/admin.py
+++ mmlog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from mmlog.models import ActivityStatusModel, ExternalMaintenanceCompanyModel, InterventionTypeModel, ComponentStatusModel, ActivitySheetModel
 
-# Register your models here.
+admin.site.register(ActivityStatusModel)
+admin.site.register(ExternalMaintenanceCompanyModel)
+admin.site.register(InterventionTypeModel)
+admin.site.register(ComponentStatusModel)
+admin.site.register(ActivitySheetModel)
+
