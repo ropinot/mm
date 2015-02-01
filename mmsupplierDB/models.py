fc3ea418623f35758944418499b9f18795a71319
Roberto <roberto@local.host>
2015-02-01 22:35:29 +0100

    ..

--- mmsupplierDB/models.py
+++ mmsupplierDB/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class ExternalCompanyStatus(models.Model):
+class ExternalCompanyStatusModel(models.Model):
         status = models.CharField(max_length=20)
 
         def __unicode__(self):
@@ -11,7 +11,7 @@ class ExternalMaintenanceCompanyModel(models.Model):
         company_name = models.CharField(max_length=200, null=True, blank=True)
         fiscal_code = models.CharField(max_length=15, null=True, blank=True)
         address = models.CharField(max_length=50, null=True, blank=True)
-        status = models.ForeignKey(ExternalCompanyStatus, null=True, blank=True)
+        status = models.ForeignKey(ExternalCompanyStatusModel, null=True, blank=True)
         registration_date = models.DateField() # timestamp
 
         def __unicode__(self):
